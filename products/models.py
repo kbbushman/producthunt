f5a33ffd58d5a93a4cf83a8f1bd78f6bbaa8e0d0
kbbushman <k.bushman@yahoo.com>
2018-08-02 22:38:34 -0700

    Adds Product MVC

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Product (models.Model):
+  def upload_path(instance, filename):
+    now = datetime.datetime.now()
+    return 'images/%s/%s/%s/%s' % (instance.hunter.username, now.year, now.month, filename)
+
+  title = models.CharField(max_length=255)
+  pub_date = models.DateTimeField()
+  body = models.TextField()
+  url = models.TextField()
+  image = models.ImageField(upload_to=upload_path)
+  icon = models.ImageField(upload_to=upload_path)
+  votes_total = models.IntegerField(default=1)
+  hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
+
+  def summary(self):
+    return self.body[:100]
+
+  def pub_date_pretty(self):
+    return self.pub_date.strftime('%b %e %Y')
--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 def upload_path(instance, filename):
     now = datetime.datetime.now()
-    return 'images/%s/%s/%s/%s' % (now.year, now.month, instance.user.username, filename)
+    return 'images/%s/%s/%s/%s' % (instance.user.username, now.year, now.month, filename)
 
 class Profile(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
